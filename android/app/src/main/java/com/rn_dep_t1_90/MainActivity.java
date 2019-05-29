b903ad941c904b2bd66bbda7db1ad4be65dc4f3c
Dan DeFelippi <dan@driverdan.com>
2019-05-29 17:25:22 +0000

    initial commit

---
+++ android/app/src/main/java/com/rn_dep_t1_90/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rn_dep_t1_90;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "rn_dep_t1_90";
+    }
+}
